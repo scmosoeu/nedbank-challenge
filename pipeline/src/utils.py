37016b166f04c3d425dc78960ff14b13031dd7a5
scmosoeu <scmosoeu@gmail.com>
2026-04-27 17:12:38 +0200

    feat: add read_csv_daa module

--- pipeline/src/utils.py
+++ pipeline/src/utils.py
@@ -2,10 +2,11 @@ import yaml
 
 from datetime import datetime
 
-from pyspark.sql import DataFrame
+from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import lit
 
 from src.logger import get_logger
+
 
 logger = get_logger(__name__)
 
@@ -34,6 +35,25 @@ def read_yaml(path: str) -> dict:
         raise yaml.YAMLError(f"Error parsing YAML file: {path}") from e
     except Exception as e:
         raise Exception(f"Error reading YAML file: {path}") from e
+
+
+def read_csv_data(session: SparkSession, path: str) -> DataFrame:
+    """
+    Read a csv file and return the contents as a dataframe
+
+    Args:
+        session: A pyspark session
+        path: The path to the csv file
+
+    Returns:
+        A spark dataframe containing the contents of
+        the csv file
+    """
+
+    logger.info(f'Reading csv file: {path}')
+    spark_df = session.read.csv(path=path, header=True)
+
+    return spark_df
 
 
 def get_datetime_now(datetime_format: str) -> str:
