bc7b9b6e99c1d3af83b9f6f1eba47b4eabc6913c
scmosoeu <scmosoeu@gmail.com>
2026-04-27 16:09:15 +0200

    feat: add datetime modules

--- pipeline/src/utils.py
+++ pipeline/src/utils.py
@@ -1,4 +1,10 @@
 import yaml
+
+from datetime import datetime
+
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import lit
+
 from src.logger import get_logger
 
 logger = get_logger(__name__)
@@ -28,3 +34,40 @@ def read_yaml(path: str) -> dict:
         raise yaml.YAMLError(f"Error parsing YAML file: {path}") from e
     except Exception as e:
         raise Exception(f"Error reading YAML file: {path}") from e
+
+
+def get_datetime_now(datetime_format: str) -> str:
+    """
+    Get the current date when the function is called
+
+    Args:
+        datetime_format: The format of how the datetime
+            field will be recorded
+    Returns:
+        The date when the pipeline ran
+    """
+    
+    datetime_now = datetime.now()
+
+    return datetime.strftime(datetime_now, datetime_format)
+
+
+def add_ingestion_timestamp(
+    df: DataFrame, 
+    ingestion_timestamp: str
+) -> DataFrame:
+    """
+    Append the ingestion timestamp to the dataframe
+
+    Args:
+        df: A spark dataframe containing contents from the 
+            data read
+        ingestion_timestamp: The time the data was ingested
+    
+    Returns:
+        A spark dataframe with an additional ingestion_timestamp
+        field added
+    """
+
+    logger.info(f"Adding the ingestion_timestamp field")
+    return df.withColumn("ingestion_timestamp", lit(ingestion_timestamp))
