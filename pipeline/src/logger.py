ba5a85c62d966c007dcec91b874bef19eb1792ab
scmosoeu <scmosoeu@gmail.com>
2026-04-27 15:18:56 +0200

    feat: add streamhandler for console massages

--- pipeline/src/logger.py
+++ pipeline/src/logger.py
@@ -1,5 +1,28 @@
 import logging
 
+
+def get_console_handler() -> logging.StreamHandler:
+    """
+    Get a console handler for displaying messages
+    on the console
+
+    Returns
+    -------
+    A stream handler to display messages on the 
+    console
+    """
+
+    formatter = logging.Formatter(
+        fmt='%(levelname)s: %(asctime)s - %(message)s', 
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+
+    return ch
+
+
 def get_logger(name: str) -> logging.Logger:
     """
     Get a logger with the given name.
@@ -10,11 +33,11 @@ def get_logger(name: str) -> logging.Logger:
     Returns:
         A logger with the given name.
     """
-    
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.setFormatter(
-        logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    )
     
+    ch = get_console_handler()
+    logger.addHandler(ch)
+
     return logger
