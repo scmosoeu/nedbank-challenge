e99cdcfbefde04ad381d60b250f0804b714f94e1
scmosoeu <scmosoeu@gmail.com>
2026-05-03 13:32:31 +0200

    chore: rename filename

--- pipeline/src/utils/data_utils.py
+++ pipeline/src/utils/data_utils.py
@@ -1,5 +1,3 @@
-import yaml
-
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import lit
 
@@ -8,33 +6,6 @@ from src.helper.logger import get_logger
 
 
 logger = get_logger(__name__)
-
-def read_yaml(path: str) -> dict:
-    """
-    Read a YAML file and return the contents as a dictionary.
-    
-    Args:
-        path: The path to the YAML file.
-
-    Returns:
-        A dictionary containing the contents of the YAML file.
-
-    Raises:
-        FileNotFoundError: If the file does not exist.
-        yaml.YAMLError: If the file is not valid YAML.  
-    """
-
-    logger.info(f"Reading YAML file: {path}")
-    try:
-        with open(path, 'r') as file:
-            return yaml.safe_load(file)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"File not found: {path}")
-    except yaml.YAMLError as e:
-        raise yaml.YAMLError(f"Error parsing YAML file: {path}") from e
-    except Exception as e:
-        raise Exception(f"Error reading YAML file: {path}") from e
-
         
 def read_csv_data(session: SparkSession, path: str) -> DataFrame:
     """
