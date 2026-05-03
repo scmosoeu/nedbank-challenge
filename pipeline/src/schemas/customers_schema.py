efdcfe135fea5dfc7956e5e25fdd438ebf6b9a2b
scmosoeu <scmosoeu@gmail.com>
2026-05-03 17:02:54 +0200

    feat: add update_customers_schema function

---
+++ pipeline/src/schemas/customers_schema.py
@@ -0,0 +1,27 @@
+import pyspark.sql.types as T
+import pyspark.sql.functions as F 
+
+from pyspark.sql import DataFrame
+
+
+def update_customers_schema(df: DataFrame) -> DataFrame:
+    """
+    Update to a standardised schema for the customers 
+    DataFrame by casting columns to their expected 
+    data types.
+
+    Args:
+        df: Input Spark DataFrame containing raw customer data.
+
+    Returns:
+        A Spark DataFrame with all columns cast to their
+        standardised schema.
+    """
+
+    standard_df = (
+        df.withColumn("dob", F.col("dob").cast(T.DateType()))
+        .withColumn("risk_score", F.col("risk_score").cast(T.IntegerType()))
+        .withColumn("ingestion_timestamp", F.col("ingestion_timestamp").cast(T.TimestampType()))
+    )
+
+    return standard_df
