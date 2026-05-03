ff6444b43ecc649c0880de9015e8d42443d14592
scmosoeu <scmosoeu@gmail.com>
2026-05-03 11:35:50 +0200

    feat: add delta feature to spark session

--- pipeline/src/sessions.py
+++ pipeline/src/sessions.py
@@ -1,3 +1,5 @@
+import os
+import delta
 from pyspark.sql import SparkSession
 
 
@@ -11,9 +13,20 @@ def get_spark_session(config: dict) -> SparkSession:
     Returns:
         A Spark session with Delta Lake support.
     """
+
+    delta_path = os.path.dirname(delta.__file__)
+    jars_path = os.path.join(delta_path, "jars", "*")
     
-    # By default, plain Apache Spark doesn’t include Delta support. You have to add it explicitly.
-    return SparkSession.builder \
-        .appName(config["spark"]["app_name"]) \
-        .master(config["spark"]["master"]) \
+    spark = (
+        SparkSession.builder 
+        .appName(config["spark"]["app_name"]) 
+        .master(config["spark"]["master"])
+        .config("spark.jars", jars_path)
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
         .getOrCreate()
+    )
+
+    # By default, plain Apache Spark doesn’t include Delta support. You have to add it explicitly.
+
+    return spark
